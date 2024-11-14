6d5ad0969a57fe1a9132d4f3bad1a9c11b1bfb79
AAL-MLOps-Shelby-S <nullterra180@gmail.com>
2024-11-13 23:30:32 -0800

    build(urlpatterns): integrate path for logout

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
+    
+    # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
 
