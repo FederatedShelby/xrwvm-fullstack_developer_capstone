47fd17dfa5eb38465180455dd269500055a7cfe0
AAL-MLOps-Shelby-S <nullterra180@gmail.com>
2024-11-14 11:03:23 -0800

    feat(getcars url): integrate getcars view with get_cars route

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    # path for registration
     path(route='register', view=views.registration, name='register'),
 
     # path for login
@@ -13,6 +13,9 @@ urlpatterns = [
     
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
+    
+    # path for cars list view
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for dealer reviews view
 
