766d5e940a83af937d9c2e0ec719094eecf4f7a9
AAL-MLOps-Shelby-S <nullterra180@gmail.com>
2024-11-14 16:16:35 -0800

    fix(undefined arg): resolve missing arg in get_dealerships fn

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -89,7 +89,7 @@ def get_cars(request):
     return JsonResponse({"CarModels":cars})
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
-def get_dealerships(request):
+def get_dealerships(request, state='All'):
     if(state == "All"):
         endpoint = "/fetchDealers"
     else:
